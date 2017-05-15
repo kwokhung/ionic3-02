6afcc068a755cec489535fdf228ee8492d086ae4
Brian Chu <brianchu@D4331.NET>
2017-05-15 16:32:24 +0800

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the HomePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage({
-   name: 'HomePage'
+  name: 'HomePage'
 })
 @Component({
   selector: 'page-home',
