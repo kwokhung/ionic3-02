d18d23b3408203b90bd4f10bb32ec54891c90c39
Brian Chu <brianchu@D4331.NET>
2017-05-16 09:34:02 +0800

    init

--- src/pages/ranger/ranger.ts
+++ src/pages/ranger/ranger.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Paho } from 'ng2-mqtt/mqttws31';
 
 @IonicPage({
   name: 'RangerPage'
@@ -9,12 +10,91 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'ranger.html',
 })
 export class RangerPage {
+  contrast: number = 0;
+  message: string;
+  client: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.client = new Paho.MQTT.Client("mbltest01.mqtt.iot.gz.baidubce.com", Number("8884"), "/mqtt", "DeviceId-s42mw9zs48");
+
+    this.client.onConnectionLost = (response) => {
+      console.log("onConnectionLost: " + response.errorCode);
+      console.log("onConnectionLost: " + response.errorMessage);
+    };
+
+    this.client.onMessageArrived = (message) => {
+      console.log("onMessageArrived: " + message.payloadString);
+      console.log(message);
+
+      this.message = message.payloadString;
+
+      let contrast: number = Number(this.message);
+
+      if (contrast >= -200 && contrast <= 200) {
+        this.contrast = contrast;
+      }
+      else if (contrast < -200) {
+        this.contrast = -200;
+      }
+      else if (contrast > 200) {
+        this.contrast = 200;
+      }
+    };
+
+    this.client.onMessageDelivered = (message) => {
+      console.log("onMessageDelivered: " + message.payloadString);
+      console.log(message);
+    };
+
+    this.client.connect({
+      useSSL: true,
+      userName: "mbltest01/letv1s01",
+      password: "hzrU8ekRn7MR7X4ycTO6OzbKbRDaaK5tmaLVY+Ue/58=",
+      invocationContext: {
+        userName: "mbltest01/letv1s01"
+      },
+      onSuccess: (context) => {
+        console.log("connect.onSuccess");
+        console.log(context);
+        console.log(this);
+
+        this.client.subscribe("letv1s01", {
+          invocationContext: {
+            topic: "letv1s01"
+          },
+          onSuccess: (response) => {
+            console.log("subscribe.onSuccess: " + response.grantedQos);
+            console.log(response.invocationContext);
+          },
+          onFailure: (response) => {
+            console.log("subscribe.onFailure: " + response.errorCode);
+            console.log(response.invocationContext);
+          }
+        });
+
+        let message: any = new Paho.MQTT.Message("Hello");
+        message.destinationName = "nodemcu01";
+
+        this.client.send(message);
+      },
+      onFailure: (response) => {
+        console.log("connect.onFailure: " + response.errorCode);
+        console.log("connect.onFailure: " + response.errorMessage);
+        console.log(response.invocationContext);
+      }
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RangerPage');
   }
 
+  slide(event) {
+    console.log(event);
+
+    let message: any = new Paho.MQTT.Message(String(event.value));
+    message.destinationName = "nodemcu01";
+
+    this.client.send(message);
+  }
 }
